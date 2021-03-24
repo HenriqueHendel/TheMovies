2d26f059a177845232c9a6d5c308c8143ec87f2c
Henrique Lopes <henriquehendell@gmail.com>
2021-03-24 14:55:12 -0300

    feat: add TMDb API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import { View, ScrollView, Text, StyleSheet, Dimensions, ImageBackground, TextInput, TouchableOpacity, Image } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
@@ -7,10 +7,12 @@ import Carousel, {CarouselProps} from 'react-native-snap-carousel'
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window')
 
 interface MovieProps {
-  title: string
-  text: string
-  release: number
-  img: string
+  id: string
+  name: string
+  overview: string
+  release_date: string
+  poster_path: string
+  [key: string]: string
 } 
 
 interface RenderItemProps {
@@ -19,55 +21,33 @@ interface RenderItemProps {
 }
 
 const App: React.FC = () => {
+
   const carouselRef = useRef(null)
 
-  const [moviesList, setMoviesList] = useState<MovieProps[]>([
-    {
-        title:"O Justiceiro",
-        text: "Após o assassinato de sua família, Frank Castle está traumatizado e sendo caçado. No submundo do crime, ele se tornará aquele conhecido como O Justiceiro",
-        release: 2018,
-        img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/background.jpg'
-    },
-    {
-        title:"Bad Boys for life",
-        text: "Terceiro episódio das histórias dos policiais Burnett (Martin Lawrence) e Lowrey (Will Smith), que devem encontrar e prender os mais perigosos traficantes de drogas da cidade.",
-        release: 2020,
-        img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/badboy.jpg'
-    },
-    {
-        title:"Viúva Negra",
-        text: "Em Viúva Negra, após seu nascimento, Natasha Romanoff (Scarlett Johansson) é dada à KGB, que a prepara para se tornar sua agente definitiva.",
-        release: 2020,
-        img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/blackwidow.jpg'
-    },
-    {
-        title:"Top Gun: MAVERICK",
-        text: "Em Top Gun: Maverick, depois de mais de 30 anos de serviço como um dos principais aviadores da Marinha, o piloto à moda antiga Maverick (Tom Cruise) enfrenta drones e prova que o fator humano ainda é fundamental no mundo contemporâneo das guerras tecnológicas.",
-        release: 2020,
-        img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/topgun.jpeg'
-    },
-    {
-        title:"BloodShot",
-        text: "Bloodshot é um ex-soldado com poderes especiais: o de regeneração e a capacidade de se metamorfosear. ",
-        release: 2020,
-        img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/blood.jpg'
-    },
-    {
-        title:"Free Guy",
-        text: "Um caixa de banco preso a uma entediante rotina tem sua vida virada de cabeça para baixo quando ele descobre que é personagem em um brutalmente realista vídeo game de mundo aberto.",
-        release: 2020,
-        img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/freeguy.jpg'
-    },
-  ])
-  const [backgroundImage, setBackgroundImage] = useState(moviesList[0].img)
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [newMovies, setNewMovies] = useState<MovieProps[]>([])
+
+  const [backgroundImage, setBackgroundImage] = useState('')
+  const [currentlySelectedMovieTitle, setCurrentlySelectedMovieTitle] = useState('')
+  const [currentlySelectedMovieOverview, setCurrentlySelectedMovieOverview] = useState('')
+  
+  useEffect(()=>{
+    (async ()=>{
+      const response = await fetch('https://api.themoviedb.org/3/discover/tv?with_network=213&language=pt-BR&page=1&api_key=f07a8ef834fe329c395643f03ebb3ade')
+      const {results} = await response.json()
+      
+      setNewMovies(results)
+      setBackgroundImage(`https://image.tmdb.org/t/p/w300/${results[0].poster_path}`)
+      setCurrentlySelectedMovieTitle(results[0].name)
+      setCurrentlySelectedMovieOverview(results[0].overview)
+    })()
+  }, [])
 
   const _renderItem = ({item, index}: RenderItemProps) => {
     return (
       <View>
         <TouchableOpacity>
           <Image 
-            source={{uri: item.img}}
+            source={{uri: `https://image.tmdb.org/t/p/w300/${item.poster_path}`}}
             style={styles.carouselImage}
           />
           <Text style={styles.carouselTitle}>
@@ -103,29 +83,30 @@ const App: React.FC = () => {
             <Text
               style={{color: '#fff', fontSize: 25, marginLeft: 10, marginVertical: 10}}
             >
-              Acabou de chegar
+              Chegarão em Breve
             </Text>
 
             <View style={styles.carouselContainer} >
               <Carousel 
                 style={styles.carousel}
                 ref={carouselRef}
-                data={moviesList}
+                data={newMovies}
                 renderItem={_renderItem}
                 sliderWidth={screenWidth}
                 itemWidth={200}
                 inactiveSlideOpacity={0.5}
                 onSnapToItem={index => {
-                  setBackgroundImage(moviesList[index].img)
-                  setActiveIndex(index)
+                 setBackgroundImage(`https://image.tmdb.org/t/p/w300/${newMovies[index].poster_path}`)
+                 setCurrentlySelectedMovieTitle(newMovies[index].name)
+                 setCurrentlySelectedMovieOverview(newMovies[index].overview)
                 }}
               />
             </View>
 
             <View style={styles.movieInfo}>
               <View style={{marginTop: 10, width: '95%'}}>
-                <Text style={styles.movieInfoTitle} >{moviesList[activeIndex].title}</Text>
-                <Text style={styles.movieInfoText}>{moviesList[activeIndex].text}</Text>
+                <Text style={styles.movieInfoTitle} >{currentlySelectedMovieTitle}</Text>
+                <Text style={styles.movieInfoText}>{currentlySelectedMovieOverview}</Text>
               </View>
 
               <TouchableOpacity style={{marginRight: 15, marginTop: 10}}>
